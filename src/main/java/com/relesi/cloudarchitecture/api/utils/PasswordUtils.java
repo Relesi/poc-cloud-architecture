f93918fb43f68d51a2918682052fe31c3573ed75
relesi <renatolessa.2011@hotmail.com>
2021-07-13 19:20:11 -0300

    imple unit test to repository company

--- src/main/java/com/relesi/cloudarchitecture/api/utils/PasswordUtils.java
+++ src/main/java/com/relesi/cloudarchitecture/api/utils/PasswordUtils.java
@@ -1,30 +1,30 @@
-package com.relesi.cloudarchitecture.api.utils;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class PasswordUtils {
-
-    private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
-
-    public PasswordUtils() {
-    }
-
-    /**
-     *
-     * Generate a hash using BCrypt.
-     *
-     * @param password
-     * @return String
-     */
-    public static String generateBCrypt(String password) {
-        if (password == null) {
-            return password;
-        }
-
-        log.info("Generate a hash with BCrypt.");
-        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
-        return bCryptEncoder.encode(password);
-    }
-}
+//package com.relesi.cloudarchitecture.api.utils;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//public class PasswordUtils {
+//
+//    private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+//
+//    public PasswordUtils() {
+//    }
+//
+//    /**
+//     *
+//     * Generate a hash using BCrypt.
+//     *
+//     * @param password
+//     * @return String
+//     */
+//    public static String generateBCrypt(String password) {
+//        if (password == null) {
+//            return password;
+//        }
+//
+//        log.info("Generate a hash with BCrypt.");
+//        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+//        return bCryptEncoder.encode(password);
+//    }
+//}
