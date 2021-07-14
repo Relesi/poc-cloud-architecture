9706912868c29002eed39d3346af17e266a98ae7
relesi <renatolessa.2011@hotmail.com>
2021-07-14 18:55:41 -0300

    test with version

--- src/main/java/com/relesi/cloudarchitecture/api/CloudArchitectureApplication.java
+++ src/main/java/com/relesi/cloudarchitecture/api/CloudArchitectureApplication.java
@@ -1,13 +1,18 @@
 package com.relesi.cloudarchitecture.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudArchitectureApplication {
+public class CloudArchitectureApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudArchitectureApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
--- src/main/java/com/relesi/cloudarchitecture/api/repositories/CompanyRepository.java
+++ src/main/java/com/relesi/cloudarchitecture/api/repositories/CompanyRepository.java
@@ -4,6 +4,7 @@ import com.relesi.cloudarchitecture.api.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     @Transactional(readOnly = true)
