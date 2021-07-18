39dae71ed1dfb5aac2e876e95fe8bdc84ae86162
relesi <renatolessa.2011@hotmail.com>
2021-07-18 11:02:49 -0300

    imple unit test to company

--- src/test/java/com/relesi/cloudarchitecture/api/services/CompanyServiceTest.java
+++ src/test/java/com/relesi/cloudarchitecture/api/services/CompanyServiceTest.java
@@ -1,8 +1,11 @@
 package com.relesi.cloudarchitecture.api.services;
 
 
+import com.relesi.cloudarchitecture.api.repositories.CompanyRepository;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,4 +13,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @ActiveProfiles("test")
 public class CompanyServiceTest {
+
+    @MockBean
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private CompanyService companyService;
+
+    private static final String EIN = "97912798000180";
 }
