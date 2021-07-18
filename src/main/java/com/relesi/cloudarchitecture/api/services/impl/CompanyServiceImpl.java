dad035c6c45e22fd65ddeca8b17f8ed696711215
relesi <renatolessa.2011@hotmail.com>
2021-07-18 10:29:20 -0300

    add persist to company

--- src/main/java/com/relesi/cloudarchitecture/api/services/impl/CompanyServiceImpl.java
+++ src/main/java/com/relesi/cloudarchitecture/api/services/impl/CompanyServiceImpl.java
@@ -2,6 +2,7 @@ package com.relesi.cloudarchitecture.api.services.impl;
 
 import java.util.Optional;
 
+import com.relesi.cloudarchitecture.api.repositories.CompanyRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,17 @@ public class CompanyServiceImpl implements CompanyService {
 
     private static final Logger log = LoggerFactory.getLogger(CompanyServiceImpl.class);
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
     @Override
     public Optional<Company> searchByEin(String ein) {
-        return Optional.empty();
+        log.info("Looking for a company for the EIN {}", ein);
+        return Optional.ofNullable(companyRepository.findByEin(ein));
     }
 
     @Override
     public Company persist(Company company) {
-        return null;
+        return companyRepository.save(company);
     }
 }
