3463c9ef3ed61e2877ded9f323d8a9c05e0217ae
relesi <renatolessa.2011@hotmail.com>
2021-07-18 10:56:04 -0300

    finish unit test to passwordUtils

--- src/test/java/com/relesi/cloudarchitecture/api/utils/PasswordUtilsTest.java
+++ src/test/java/com/relesi/cloudarchitecture/api/utils/PasswordUtilsTest.java
@@ -4,14 +4,22 @@ import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class PasswordUtilsTest {
 
-    private static final String SENHA = "123456";
+    private static final String PASSWORD = "123456";
     private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     @Test
-    public void testNullPassword() throws Exception{
+    public void testNullPassword() throws Exception {
         assertNull(PasswordUtils.generateBCrypt(null));
+    }
+
+    @Test
+    public void testGeneratePasswordHash() throws Exception {
+        String hash = PasswordUtils.generateBCrypt(PASSWORD);
+        assertTrue(bCryptPasswordEncoder.matches(PASSWORD, hash));
+
     }
 }
