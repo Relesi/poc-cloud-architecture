a276486fa7a8292f2aca8e56af7e54db331a132e
relesi <renatolessa.2011@hotmail.com>
2021-07-19 14:41:21 -0300

    unit test to company

--- src/test/java/com/relesi/cloudarchitecture/api/services/CompanyServiceTest.java
+++ src/test/java/com/relesi/cloudarchitecture/api/services/CompanyServiceTest.java
@@ -4,6 +4,7 @@ package com.relesi.cloudarchitecture.api.services;
 import com.relesi.cloudarchitecture.api.entities.Company;
 import com.relesi.cloudarchitecture.api.repositories.CompanyRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
@@ -12,6 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,4 +36,20 @@ public class CompanyServiceTest {
         BDDMockito.given(this.companyRepository.findByEin(Mockito.anyString())).willReturn(new Company());
         BDDMockito.given(this.companyRepository.save(Mockito.any(Company.class))).willReturn(new Company());
     }
+
+    @Test
+    public void testSearchCompanyByEin(){
+        final Optional<Company> company = this.companyService.searchByEin(EIN);
+        assertTrue(company.isPresent());
+    }
+
+    @Test
+    public void testPersistCompany(){
+         Company company = this.companyService.persist(new Company());
+         assertNotNull(company);
+
+    }
+
+
+
 }
