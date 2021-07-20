7c921ecb0a96ac1519c10ca999c76cc89f58f89c
relesi <renatolessa.2011@hotmail.com>
2021-07-20 11:53:33 -0300

    finish test and service to launched

--- src/test/java/com/relesi/cloudarchitecture/api/services/LaunchedServiceTest.java
+++ src/test/java/com/relesi/cloudarchitecture/api/services/LaunchedServiceTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
@@ -42,6 +43,11 @@ public class LaunchedServiceTest {
         BDDMockito.given(this.launchedRepository.save(Mockito.any(Launched.class))).willReturn(new Launched());
     }
 
+    @Test
+    public void testSearchLaunchedByEmployeeId(){
+        Page<Launched> launched = this.launchedService.searchByEmployeeId(1L, new PageRequest(0, 10));
+        assertNotNull(launched);
+    }
 
 
 
