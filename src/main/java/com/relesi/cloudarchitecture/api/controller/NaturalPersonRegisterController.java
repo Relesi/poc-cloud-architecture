ee8a6565306f096d1ab035ea2841446a4db6b98a
relesi <renatolessa.2011@hotmail.com>
2021-07-21 16:25:11 -0300

    init creating to NaturaPerson

---
+++ src/main/java/com/relesi/cloudarchitecture/api/controller/NaturalPersonRegisterController.java
@@ -0,0 +1,9 @@
+package com.relesi.cloudarchitecture.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/natural-person")
+public class NaturalPersonRegisterController {
+}
