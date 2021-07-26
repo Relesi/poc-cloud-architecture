2806e24c58873e13b1cc6f8856a19e86d4418afa
relesi <renatolessa.2011@hotmail.com>
2021-07-26 14:21:05 -0300

    impl controller to company

--- src/main/java/com/relesi/cloudarchitecture/api/controller/CompanyContreller.java
+++ src/main/java/com/relesi/cloudarchitecture/api/controller/CompanyContreller.java
@@ -2,6 +2,7 @@ package com.relesi.cloudarchitecture.api.controller;
 
 import com.relesi.cloudarchitecture.api.Response.Response;
 import com.relesi.cloudarchitecture.api.dtos.CompanyDto;
+import com.relesi.cloudarchitecture.api.entities.Company;
 import com.relesi.cloudarchitecture.api.entities.Employee;
 import com.relesi.cloudarchitecture.api.services.CompanyService;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/company")
@@ -26,21 +28,30 @@ public class CompanyContreller {
     public CompanyContreller(){
 
     }
-    @GetMapping(value = "/cnpj/{cnpj}")
+
+
+    @GetMapping(value = "/ein/{ein}")
     public ResponseEntity<Response<CompanyDto>> searchByEin(@PathVariable("ein") String ein){
         log.info("Search Company by EIN: {}", ein);
 
+        Response<CompanyDto> response = new Response<CompanyDto>();
+        Optional<Company> company = companyService.searchByEin(ein);
 
+        if (!company.isPresent()) {
+            log.info("Company not found for the EIN: {}", ein);
+            response.getErrors().add("Company not found for the EIN " + ein);
+            return ResponseEntity.badRequest().body(response);
+        }
 
-        return null;
+        response.setData(this.convertCompanyDto(company.get()));
+        return ResponseEntity.ok(response);
 
     }
 
-
-
-
-
-
+    private CompanyDto convertCompanyDto(Company company) {
+        //TODO
+        return null;
+    }
 
 
 }
