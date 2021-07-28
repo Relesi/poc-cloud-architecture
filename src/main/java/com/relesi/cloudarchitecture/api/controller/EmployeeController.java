0128ab762f8ab47b36f0eea99b7729da7281c374
relesi <renatolessa.2011@hotmail.com>
2021-07-28 17:56:13 -0300

    validating emplly

--- src/main/java/com/relesi/cloudarchitecture/api/controller/EmployeeController.java
+++ src/main/java/com/relesi/cloudarchitecture/api/controller/EmployeeController.java
@@ -2,16 +2,19 @@ package com.relesi.cloudarchitecture.api.controller;
 
 import com.relesi.cloudarchitecture.api.Response.Response;
 import com.relesi.cloudarchitecture.api.dtos.EmployeeDto;
+import com.relesi.cloudarchitecture.api.entities.Employee;
 import com.relesi.cloudarchitecture.api.services.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employee")
@@ -27,13 +30,56 @@ public class EmployeeController {
 
     }
 
+    /***
+     * Updates Data of an employee
+     *
+     * @param id
+     * @param employeeDto
+     * @param result
+     * @return ResponseEntity<Response<EmployeeDto>>
+     * @throws NoSuchAlgorithmException
+     */
+    @PutMapping(value = "/{id}")
     public ResponseEntity<Response<EmployeeDto>> update(@PathVariable("id") Long id,
                                                         @Valid @RequestBody EmployeeDto employeeDto, BindingResult result) throws NoSuchAlgorithmException{
 
-        //TODO
-        return null;
+        log.info("Employee Update: {}", employeeDto.toString());
+        Response<EmployeeDto> response = new Response<EmployeeDto>();
+
+        Optional<Employee> employee = this.employeeService.searchById(id);
+        if (!employee.isPresent()) {
+            result.addError(new ObjectError("employee", "Employee not found."));
+        }
+
+        this.updateEmployeeData(employee.get(), employeeDto, result);
+
+        if (result.hasErrors()) {
+            log.error("Error validating employee: {}", result.getAllErrors());
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        this.employeeService.persist(employee.get());
+        response.setData(this.convertEmployeeToDto(employee.get()));
+
+        return ResponseEntity.ok(response);
     }
 
+    private void updateEmployeeData(Employee employee, EmployeeDto employeeDto, BindingResult result) throws NoSuchAlgorithmException {
+        
+        employee.setName(employeeDto.getName());
+
+        if(!employee.getEmail().equals(employeeDto.getEmail())){
+            this.employeeService.searchByEmail(employeeDto.getEmail())
+                    .ifPresent(empl -> result.addError(new ObjectError("emial", "Existing Email")));
+            employee.setEmail(employeeDto.getEmail());
+        }
+    }
+
+    private EmployeeDto convertEmployeeToDto(Employee employee) {
+
+        return null;
+    }
 
 
 }
