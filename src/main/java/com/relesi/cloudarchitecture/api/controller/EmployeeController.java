6ed9b1120054c817075c88fa4ffb9d16bf8e29f2
relesi <renatolessa.2011@hotmail.com>
2021-07-28 14:34:03 -0300

    add init impl to employee

---
+++ src/main/java/com/relesi/cloudarchitecture/api/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.relesi.cloudarchitecture.api.controller;
+
+import com.relesi.cloudarchitecture.api.services.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employee")
+@CrossOrigin(origins = "*")
+public class EmployeeController {
+
+    private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    public EmployeeController(){
+
+    }
+
+
+}
