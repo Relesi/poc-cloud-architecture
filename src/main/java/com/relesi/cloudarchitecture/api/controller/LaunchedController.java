e3d66fec8aa69c8d6afcee58cec1e578eafc9c76
relesi <renatolessa.2011@hotmail.com>
2021-07-31 17:38:29 -0300

    init controller to launcehd

--- src/main/java/com/relesi/cloudarchitecture/api/controller/LaunchedController.java
+++ src/main/java/com/relesi/cloudarchitecture/api/controller/LaunchedController.java
@@ -1,9 +1,12 @@
 package com.relesi.cloudarchitecture.api.controller;
 
 import com.relesi.cloudarchitecture.api.dtos.LaunchedDto;
+import com.relesi.cloudarchitecture.api.entities.Employee;
 import com.relesi.cloudarchitecture.api.entities.Launched;
+import com.relesi.cloudarchitecture.api.enums.TypeEnum;
 import org.springframework.data.domain.Sort.Direction;
 
+import org.apache.commons.lang3.EnumUtils;
 import com.relesi.cloudarchitecture.api.Response.Response;
 import com.relesi.cloudarchitecture.api.services.EmployeeService;
 import com.relesi.cloudarchitecture.api.services.LaunchedService;
@@ -14,8 +17,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Optional;
 
@@ -42,6 +49,15 @@ public class LaunchedController {
 
     }
 
+    /***
+     * Return listing of launched of a employee.
+     *
+     * @param employeeId
+     * @param pag
+     * @param ord
+     * @param dir
+     * @return
+     */
     @GetMapping(value = "/employee/{employeeId}")
     public ResponseEntity<Response<Page<LaunchedDto>>> listByEmployeeId(
             @PathVariable("employeeId") Long employeeId,
@@ -61,6 +77,112 @@ public class LaunchedController {
 
 
     }
+
+    /***
+     * Return a launched by ID.
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Response<LaunchedDto>> listById(@PathVariable("id") Long id){
+        log.info("Search launched by ID: {}", id);
+        Response<LaunchedDto> response = new Response<LaunchedDto>();
+        Optional<Launched> launched = this.launchedService.searchById(id);
+
+        if (!launched.isPresent()) {
+            log.info("Not found Launched for ID: {}", id);
+            response.getErrors().add("Not found Launched for ID: {} " + id);
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        response.setData(this.convertLaunchedDto(launched.get()));
+        return ResponseEntity.ok(response);
+    }
+
+    /***
+     * Adds a new Launched.
+     *
+     * @param launchedDto
+     * @param result
+     * @return
+     * @throws ParseException
+     */
+    @PostMapping
+    public ResponseEntity<Response<LaunchedDto>> add(@Valid @RequestBody LaunchedDto launchedDto,
+                                                     BindingResult result) throws ParseException{
+        log.info("Launched adding.: {}", launchedDto.toString());
+        Response<LaunchedDto> response = new Response<LaunchedDto>();
+        validateEmployee(launchedDto, result);
+        Launched launched = this.convertDtoToLaunched(launchedDto, result);
+
+        if (result.hasErrors()) {
+            log.error("Error validating Launched: {}", result.getAllErrors());
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+        
+        launched = this.launchedService.persist(launched);
+        response.setData(this.convertLaunchedDto(launched));
+        return ResponseEntity.ok(response);
+
+    }
+
+    /***
+     * Convert a LaunchedDto to a entity Launched.
+     *
+     * @param launchedDto
+     * @param result
+     * @return
+     * @throws ParseException
+     */
+    private Launched convertDtoToLaunched(LaunchedDto launchedDto, BindingResult result) throws ParseException {
+        Launched launched = new Launched();
+
+        if (launchedDto.getId().isPresent()) {
+            Optional<Launched> lanc = this.launchedService.searchById(launchedDto.getId().get());
+            if (lanc.isPresent()) {
+                launched = lanc.get();
+            }else {
+                result.addError(new ObjectError("launched", "Launched not found"));
+            }
+        }else {
+            launched.setEmployee(new Employee());
+            launched.getEmployee().setId(launchedDto.getEmployeeId());
+        }
+
+        launched.setDescription(launchedDto.getDescription());
+        launched.setLocalization(launchedDto.getLocalization());
+        launched.setCurrentDate(this.dateFormat.parse(launchedDto.getData()));
+
+        if (EnumUtils.isValidEnum(TypeEnum.class, launchedDto.getType())) {
+            launched.setType(TypeEnum.valueOf(launchedDto.getType()));
+        }else {
+            result.addError(new ObjectError("type", "Invalid type."));
+        }
+        return launched;
+    }
+
+    /***
+     * Validates an employee, verifying that he is existing and valid in the system.
+     *
+     * @param launchedDto
+     * @param result
+     */
+    private void validateEmployee(LaunchedDto launchedDto, BindingResult result) {
+        if (launchedDto.getEmployeeId() == null) {
+            result.addError(new ObjectError("employee", "Employee not informed."));
+            return;
+        }
+
+        log.info("Validating employee ID {}: ", launchedDto.getEmployeeId());
+        Optional<Employee> employee = this.employeeService.searchById(launchedDto.getEmployeeId());
+        if (!employee.isPresent()) {
+            result.addError(new ObjectError("employee", "Employee not found non-existent ID."));
+
+        }
+    }
+
 
     /***
      * Convert a entity Launched to your respective DTO.
